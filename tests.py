f64f5ba7b8b10aba8ae83313ab9a5ce76eda7298
jvmaia <joaovmferreira@gmail.com>
2018-03-30 21:06:51 -0300

    add tests for json array

--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
 from org.json import JSONObject
 import PythonJavaJSON
 
-print(PythonJavaJSON.dumps([{}, {}]))
+obj = "{'a':5, 'b':10, 'c':'teste', 'd':'iasudfnjskd', 'e':null}"
+obj_json = PythonJavaJSON.loads(obj)
+print(type(obj_json))
+print(obj_json, end='\n\n')
 
+array = """[{'a':5, 'b':10, 'c':'teste', 'd':'bbbbbbbb', 'e':null},
+            {'a':5, 'b':10, 'c':'teste', 'd':'cccccc', 'e':null},
+            {'a':5, 'b':10, 'c':'teste', 'd':'eeeee', 'e':null }]"""
+array_json = PythonJavaJSON.loads(array)
+print(type(array_json))
+print(array_json, end='\n\n')
