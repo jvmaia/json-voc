934d6f266209dbfd329003edb2225df4d2a288d0
jvmaia <joaovmferreira@gmail.com>
2018-03-30 21:07:29 -0300

    add support for json array

--- PythonJavaJSON.py
+++ PythonJavaJSON.py
@@ -1,7 +1,7 @@
-from org.json import JSONObject
+from org.json import JSONObject, JSONArray
 
 
-def getKeys(jsonObject):
+def getKeysObject(jsonObject):
     keys = []
     keysIterator = jsonObject.keySet().iterator()
     while keysIterator.hasNext():
@@ -9,4 +9,39 @@ def getKeys(jsonObject):
 
     return keys
 
-print(getKeys(JSONObject('{"a":235, "b":null, "c":235, "z":15, "15":30}')))
+
+def getKeysArray(jsonArray):
+    length = jsonArray.length()
+    keys = [i for i in range(length)]
+    return keys
+
+
+def jsonToDict(json):
+    keys = getKeysObject(json)
+    json_dict = {}
+    for key in keys:
+        if json.isNull(key):
+            value = None
+        else:
+            value = json.get(key)
+        json_dict[key] = value
+
+    return json_dict
+
+
+def loads(s):
+    if s.startswith('{'):
+        objJson = JSONObject(s)
+        python_json = jsonToDict(objJson)
+        return python_json
+    else:
+        json = JSONArray(s)
+        keys = getKeysArray(json)
+        python_json = []
+
+    for key in keys:
+        objJson = json.get(key)
+        objDict = jsonToDict(objJson)
+        python_json.append(objDict)
+
+    return python_json
