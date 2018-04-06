ff36f0e09841a5d3b2d0da64a3175f8c6f70e8d3
jvmaia <joaovmferreira@gmail.com>
2018-04-06 11:59:38 -0300

    update tests

--- testsDumps.py
+++ testsDumps.py
@@ -3,8 +3,8 @@ import PythonJavaJSON
 
 # TESTS FOR FUNCTION DUMPS()
 
-# JSON OBJECT TEST
 
+print('SIMPLE JSON OBJECT TEST')
 o = {
     'a': {'b': 5},
     'd': {'e': 10},
@@ -14,31 +14,29 @@ o = {
 }
 o_dump = PythonJavaJSON.dumps(o)
 print(type(o_dump))
-print(o_dump, end='\n\n\n')
+print(o_dump, end='\n\n')
 
 
-# JSON ARRAY TEST
-
+print('SIMPLE JSON ARRAY TEST')
 a = [
+    {
+        'a': [{'b': 5, 'c': False}],
+        'd': {'e': True, 'f': None},
+        'g': [
+            {'h': [{'i': 99, 'j': 'test'}]},
+            {'k': 9.7}
+        ]
+    },
+    [
+        {'a0': 0},
         {
-            'a': [{'b':5, 'c':False}],
-            'd': {'e': True, 'f':None},
-            'g': [
-                {'h': [{'i':99, 'j':'test'}]},
-                {'k': 9.7}
-                ]
-        },
-        [
-            {'a0': 0},
-            {
-                'b0': [
+            'b0': [
                 {'c0': 329485},
                 {'d0': None}
-                ]
-            }
-        ]
+            ]
+        }
     ]
+]
 a_dump = PythonJavaJSON.dumps(a)
 print(type(a_dump))
 print(a_dump)
-
--- testsLoads.py
+++ testsLoads.py
@@ -3,14 +3,15 @@ import PythonJavaJSON
 
 # TESTS FOR FUNCTION LOADS()
 
-# SIMPLE JSON TEST
+
+print('SIMPLE JSON TEST')
 obj = "{'a':6, 'b':10, 'c':'teste', 'd':'iasudfnjskd', 'e':null}"
 obj_json = PythonJavaJSON.loads(obj)
 print(type(obj_json))
 print(obj_json, end='\n\n')
 
 
-# SIMPLE JSON ARRAY TEST
+print('SIMPLE JSON ARRAY TEST')
 array = """[{'a':5, 'b':10, 'c':'test', 'd':'bbbbbbbb', 'e':null},
             {'a':5, 'b':10, 'c':'test', 'd':'cccccc'},
             {'a':5, 'b':10, 'c':'test'}]
@@ -19,14 +20,15 @@ array_json = PythonJavaJSON.loads(array)
 print(type(array_json))
 print('ARRAY 00 --> ', array_json, end='\n\n')
 
-# NESTED JSON ARRAY TEST 02
+
+print('NESTED JSON ARRAY TEST 02')
 array01 = """[{"h": null}, {"j":2345}, [{"k":9.0}]]"""
 array01_json = PythonJavaJSON.loads(array01)
 print(type(array01_json))
 print('ARRAY 01 --> ', array01_json, end='\n\n')
 
 
-# EVERYTHING NESTED JSON OBJECT/ARRAY  TEST
+print('NESTED JSON OBJECT AND ARRAY')
 nested = """
         [{"test": [{"t": 0, "e": 1}, [{"s":2}, {"t": 3}]]},
         {"test2": {"s": 2, "t": true, "f": false } }]
